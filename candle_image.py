3df99879859d6f02257ca7327222af61ff7ba9e0
Yu Chen <130757322+AmatherCY@users.noreply.github.com>
2025-09-05 10:13:54 +0800

    Update candle_image.py

--- candle_image.py
+++ candle_image.py
@@ -26,12 +26,12 @@ points=np.array(points)
 #points=np.unique(points, axis=0) 
 print(len(points))
 
-for k in range(0,points.shape[1]): #数据归一化
+for k in range(0,points.shape[1]):
     points[:,k]=(points[:,k]-min(points[:,k]))/(max(points[:,k])-min(points[:,k]))
 
 plot=plt.scatter(points[:,0], points[:,1],s=5)
 plt.gcf().set_size_inches(10,6) 
-plt.axis('off')  # 关闭坐标轴
+plt.axis('off') 
 plt.show()
 
 
@@ -50,8 +50,6 @@ gd.plot_persistence_diagram(diag,legend=True)
 plt.gcf().set_size_inches(8, 6) 
 plt.show()
 
-
-#普通持续图
 dgm_data0=[]
 dgm_data1=[]
 for pd in diag:
@@ -63,22 +61,22 @@ dgm_data=[dgm_data0,dgm_data1]
 
 
 pt_list=dgm_data0
-pt_list[0][1] = max_dis  #替换inf值
+pt_list[0][1] = max_dis 
 
 connect_num=s.cluster_0PD(diag,pt_list)
 
 def custom_compare(item):  
     return item[1] - item[0] 
-pt_list=np.array(sorted(pt_list, key=custom_compare, reverse=True)) #按persistence从大到小排序
+pt_list=np.array(sorted(pt_list, key=custom_compare, reverse=True)) 
 
 threshold0=0.0
 per_sup=pt_list[connect_num-1][1]-pt_list[connect_num-1][0] 
-pd_selected=[]  #筛选出持续时间大于 per_sup 的0PD
+pd_selected=[] 
 for i in range(len(pt_list)):
     if pt_list[i][1]-pt_list[i][0]>=per_sup:
         pd_selected.append(pt_list[i])
 
-if connect_num==1:  #只有一个连通分支
+if connect_num==1:  
     threshold = pt_list[0][1]*1.01
 else:
     threshold = pt_list[connect_num][1]
@@ -90,17 +88,17 @@ adjacency_matrix = csr_matrix((n, n), dtype=np.float32)
 if Method=='VR':
     for i in range(n):  
         for j in range(i+1, n):  
-            dist = distance.euclidean(points[i], points[j])  # 假设使用欧氏距离  
-            if dist <= threshold:  # 根据权重阈值来判断是否连接  
+            dist = distance.euclidean(points[i], points[j])  
+            if dist <= threshold:  
                 adjacency_matrix[i, j] = dist  
-                adjacency_matrix[j, i] = dist  # 无向图，对称矩阵
+                adjacency_matrix[j, i] = dist  
 
 if Method=='alpha':
     for f in Filt:
         if len(f[0])==2:
             i,j = f[0][0],f[0][1]
             
-            if f[1] <= threshold:  # 根据权重阈值来判断是否连接 
+            if f[1] <= threshold:  
                 dist=distance.euclidean(points[i], points[j])
                 adjacency_matrix[i,j]=dist
                 adjacency_matrix[j,i]=dist
@@ -143,25 +141,23 @@ import numpy as np
 from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
 from scipy.spatial import distance
-# 函数：寻找并绘制符合条件的三角形
+
 def find_and_plot_triangles(points, region_condition, color):
     selected_points = points
     tri = Delaunay(selected_points)
     valid_triangles = []
 
-    # 寻找符合条件的三角形
     for indices in tri.simplices:
         side_lengths = [distance.euclidean(selected_points[indices[i]], selected_points[indices[(i + 1) % 3]]) for i in range(3)]
         if all(length < 0.25 for length in side_lengths):
             valid_triangles.append(indices)
 
-    # 绘制符合条件的三角形
     for indices in valid_triangles:
         triangle_points = selected_points[indices]
         if np.all(region_condition(triangle_points)):
             plt.fill(triangle_points[:, 0], triangle_points[:, 1], c=color)
 
-# 定义区域条件
+
 region10 = lambda points: (points[:, 0] < 0.09)
 region11 = lambda points: (0.09 < points[:, 0]) & (points[:, 0] < 0.2)
 region20 = lambda points: (0.4 < points[:, 0]) & (points[:, 0] < 0.53)
@@ -169,7 +165,7 @@ region30 = lambda points: ((0.6 < points[:, 0]) & (points[:, 0] < 0.76))
 region31 = lambda points: ((0.76 < points[:, 0]) & (points[:, 0] < 0.872))
 region32 = lambda points: (points[:, 0] > 0.872)
 
-# 应用函数并绘制图形
+
 find_and_plot_triangles(points[pw1], region10, 'red')
 find_and_plot_triangles(points[pw1], region11, 'red')
 find_and_plot_triangles(points[pw2], region20, 'green')
@@ -178,7 +174,7 @@ find_and_plot_triangles(points[pw3], region31, 'blue')
 find_and_plot_triangles(points[pw3], region32, 'blue')
 
 plt.gcf().set_size_inches(10,6)  
-plt.axis('off')  # 关闭坐标轴  
+plt.axis('off')
 plt.show()  
 
 
@@ -188,42 +184,33 @@ import numpy as np
 from scipy.spatial import Delaunay
 from scipy.spatial.distance import euclidean
 
-# 读取原始图片
 image_path = 'pic\\candle2\\candle2.PNG'
 image = Image.open(image_path)
 
-# 创建一个可以在图像上绘图的对象
 draw = ImageDraw.Draw(image)
 
-# 加载点数据
 points = np.loadtxt('data//candle2.txt')
 width, height = image.size
 
-# 创建三角剖分和可视化
 def plot_delaunay_on_image(draw, points, region, color):
-    # 仅考虑 pw 中的点
     selected_points = points
     selected_points[:,1]=-selected_points[:,1]
-    # 计算 Delaunay 三角剖分
     tri = Delaunay(selected_points)
 
-    # 存储符合条件的三角形的索引
     valid_triangles = []
 
-    # 寻找符合条件的三角形
     for indices in tri.simplices:
         side_lengths = [euclidean(selected_points[indices[i]], selected_points[indices[(i + 1) % 3]]) for i in range(3)]
         if all(length < 100 for length in side_lengths):
             valid_triangles.append(indices)
 
-    # 绘制符合条件的三角形
     for indices in valid_triangles:
         triangle_points = selected_points[indices]
         triangle = [tuple(selected_points[i]) for i in indices]
         if np.all(region(triangle_points)):
             draw.polygon(triangle, fill=color, outline=color)
 
-# 定义区域条件
+
 region10 = lambda points: (points[:, 0] < 310)
 region11 = lambda points: ((310 < points[:, 0]) & (points[:, 0] < 400))
 region20 = lambda points: (400 < points[:, 0]) & (points[:, 0] < 550)
@@ -231,13 +218,12 @@ region30 = lambda points: ((600 < points[:, 0]) & (points[:, 0] < 655))
 region31 = lambda points: ((655 < points[:, 0]) & (points[:, 0] <= 703))
 region32 = lambda points: (points[:, 0] > 703)
                          
-# 绘制分区域的 Delaunay 三角剖分
 plot_delaunay_on_image(draw, points[pw1], region10, 'red')
 plot_delaunay_on_image(draw, points[pw1], region11, 'red')
 plot_delaunay_on_image(draw, points[pw2], region20, 'green')
 plot_delaunay_on_image(draw, points[pw3], region30, 'blue')
 plot_delaunay_on_image(draw, points[pw3], region31, 'blue')
 plot_delaunay_on_image(draw, points[pw3], region32, 'blue')
-# 保存新的图片
+
 image.save("new_candle2.png")
 '''
