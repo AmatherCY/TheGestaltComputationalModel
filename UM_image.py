1daf73c9dc63dab767cd9faeb8eac11985321b91
Yu Chen <130757322+AmatherCY@users.noreply.github.com>
2025-09-05 10:12:05 +0800

    Update UM_image.py

--- UM_image.py
+++ UM_image.py
@@ -29,14 +29,14 @@ def draw_all_curves(cycles,points):
         k=k+1
 
     
-    plt.axis('off')  # 关闭坐标轴
+    plt.axis('off') 
     plt.gcf().set_size_inches(8,6) 
     plt.show()
 
 max_dis=10
 p = np.loadtxt('data\\UM1.txt')
 
-for k in range(0,p.shape[1]): #数据归一化
+for k in range(0,p.shape[1]): 
     if k>1:
         p[:,k]=max_dis * (p[:,k] - np.min(p[:,k])) / (np.max(p[:,k]) - np.min(p[:,k]))
     else:
@@ -46,13 +46,13 @@ for k in range(0,p.shape[0]):
 for k in range(2,p.shape[1]):       
     for i in range(p.shape[0]):
         p[i,k]= max_dis if p[i,k]>max_dis*0.5 else 0
-'''     
+ 
 plot=plt.scatter(p[:,0], p[:,1], c=p[:,2])
 plt.colorbar(plot)
 plt.gcf().set_size_inches(8,6) 
 plt.axis('equal')
 plt.show()
-'''
+
 import gudhi as gd
 #alp = gd.AlphaComplex(points=np.array(p))
 #simplex_tree = alp.create_simplex_tree()
@@ -60,13 +60,12 @@ rips = gd.RipsComplex(points=np.array(p), max_edge_length = max_dis+1)
 simplex_tree = rips.create_simplex_tree(max_dimension=2)
 diag = simplex_tree.persistence(homology_coeff_field=2, min_persistence=0)
 
-'''
+
 gd.plot_persistence_diagram(diag,legend=True)
 plt.gcf().set_size_inches(8, 6) 
 plt.show() 
-'''
 
-start = time.time()
+
 import ripser
 dgm_data=ripser.ripser(p)['dgms']
 
@@ -136,9 +135,9 @@ np.savetxt('data\\pu1.txt',p1,fmt='%2f')
 p2=np.array(p2)
 np.savetxt('data\\pu2.txt',p2,fmt='%2f')
 
-#3d 图像       
+      
 from mpl_toolkits.mplot3d import Axes3D  
-'''
+
 fig = plt.figure()  
 ax = fig.add_subplot(111, projection='3d')  
   
@@ -149,7 +148,7 @@ for i in range(n):
         if adjacency_matrix[i, j] != 0:  
             ax.plot([points[i, 0], points[j, 0]], [points[i, 1], points[j, 1]], [points[i, 2], points[j, 2]], 'y-')  
 plt.show()  
-'''
+
 
 
 points=np.loadtxt('data\\pu2.txt')[:,:2]
@@ -175,12 +174,12 @@ gd.plot_persistence_diagram(diag,legend=True)
 plt.gcf().set_size_inches(8, 6) 
 plt.show()
 '''
-#获取环的指标
+
 st=simplex_tree
 indices=s.get_all_1dgms(st,points)
 #print(indices)
 
-#普通持续图
+
 dgm_data0=[]
 dgm_data1=[]
 for pd in diag:
@@ -198,17 +197,17 @@ adjacency_matrix = csr_matrix((n, n), dtype=np.float32)
 if Method=='VR':
     for i in range(n):  
         for j in range(i+1, n):  
-            dist = distance.euclidean(points[i], points[j])  # 假设使用欧氏距离  
-            if dist <= threshold:  # 根据权重阈值来判断是否连接  
+            dist = distance.euclidean(points[i], points[j]) 
+            if dist <= threshold: 
                 adjacency_matrix[i, j] = dist  
-                adjacency_matrix[j, i] = dist  # 无向图，对称矩阵
+                adjacency_matrix[j, i] = dist 
 
 if Method=='alpha':
     for f in Filt:
         if len(f[0])==2:
             i,j = f[0][0],f[0][1]
             
-            if f[1] <= threshold:  # 根据权重阈值来判断是否连接 
+            if f[1] <= threshold:  
                 dist=distance.euclidean(points[i], points[j])
                 adjacency_matrix[i,j]=dist
                 adjacency_matrix[j,i]=dist
@@ -221,12 +220,12 @@ for i in range(n):
         if adjacency_matrix[i, j] != 0:  
              G.add_edges_from([(i,j)])
 
-#最小转角算法
+
 edges=[[] for _ in range(len(indices))]
 visit=[]
 cycle_list=[]
 k = 0
-while len(cycle_list)<loop_num and k<len(indices): #找满loop_num个环
+while len(cycle_list)<loop_num and k<len(indices):
     edge = sorted(indices[k])
     start_vertex, end_vertex = edge[0], edge[1]
     print(start_vertex, end_vertex)
@@ -234,10 +233,9 @@ while len(cycle_list)<loop_num and k<len(indices): #找满loop_num个环
         cycle=s.dfs_min_angle_path(G,start_vertex,end_vertex,points)
         #print(cycle)
         if cycle is not None:  
-            #if s.not_similar(cycle,cycle_list,points,haus_thres=0.05):  #用Hausdorff距离判断两个环是否相似
+            #if s.not_similar(cycle,cycle_list,points,haus_thres=0.05): 
             cycle_list.append(cycle)
                           
     k=k+1
-#draw_all_curves(cycle_list,points)
-end = time.time()
-print(f"Time: {end - start}s")
+draw_all_curves(cycle_list,points)
+
