0d304a97b372a4c83f21794229f37b842a876b33
Yu Chen <130757322+AmatherCY@users.noreply.github.com>
2025-09-05 12:29:47 +0800

    Update waves_image.py

--- waves_image.py
+++ waves_image.py
@@ -5,7 +5,6 @@ from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import depth_first_order  
 from scipy.sparse.csgraph import connected_components  
 import search_funcs as s
-import tadasets
 import warnings  
 warnings.filterwarnings("ignore")  
 import math  
@@ -13,18 +12,19 @@ from scipy.sparse import csr_matrix
 import gudhi as gd
 import networkx as nx
 
-#step_size = 20 
+#step_size = 20
 
 max_dis=10
-p = np.loadtxt('data\\waves111.txt')
+p = np.loadtxt('data/waves111.txt')
 
-for k in range(0,p.shape[1]): 
+for k in range(0,p.shape[1]):
     if k>1:
         p[:,k]=max_dis * (p[:,k] - np.min(p[:,k])) / (np.max(p[:,k]) - np.min(p[:,k]))
     else:
         p[:,k]=(p[:,k]-min(p[:,k]))/(max(p[:,k])-min(p[:,k]))
 for k in range(0,p.shape[0]):
     p[k,1]=1-p[k,1]
+
      
 plot=plt.scatter(p[:,0], p[:,1], c=p[:,2])
 plt.colorbar(plot)
@@ -61,7 +61,6 @@ pt_list[np.isinf(pt_list)] = max_death + delta
 cluster_num=s.cluster_0PD(diag,dgm_data[0])
 
 pt_list=np.array(sorted(pt_list, key=s.custom_compare, reverse=True))
-threshold1, loop_num = s.get_threshold_1PDonly(points,dgm_data,diag,Method='PDcluster')
 
 threshold=0.0
 per_sup=pt_list[cluster_num-1][1]-pt_list[cluster_num-1][0]
@@ -73,7 +72,6 @@ if cluster_num == 1:
     threshold = pt_list[1][1]
 else:
     threshold = pt_list[cluster_num][1]
-threshold=max(threshold,threshold1)*1.01
 print(threshold)
 
 n = len(points)  
@@ -91,7 +89,7 @@ for i in range(n):
         if adjacency_matrix[i, j] != 0:  
              G.add_edges_from([(i,j)])
 
-       
+   
 connected_components = list(nx.connected_components(G))      
             
 pw1=[]
@@ -111,8 +109,7 @@ p1=np.array(p1)
 np.savetxt('data\\pw1.txt',p1,fmt='%2f')
 p2=np.array(p2)
 np.savetxt('data\\pw2.txt',p2,fmt='%2f')
-
-    
+  
 from mpl_toolkits.mplot3d import Axes3D  
   
 fig = plt.figure()  
@@ -133,68 +130,6 @@ plt.gcf().set_size_inches(8,6)
 plt.axis('equal')
 plt.show()
 
-
-Method='VR'
-
-if Method=='VR':
-    rips = gd.RipsComplex(points=np.array(points), max_edge_length = max_dis)
-    simplex_tree = rips.create_simplex_tree(max_dimension=2)
-if Method=='alpha':
-    alp = gd.AlphaComplex(points=np.array(points))
-    simplex_tree = alp.create_simplex_tree()
-diag = simplex_tree.persistence()
-Filt = list(simplex_tree.get_filtration())
-
-gd.plot_persistence_diagram(diag,legend=True)
-plt.gcf().set_size_inches(8, 6) 
-plt.show()
-
-
-st=simplex_tree
-indices=s.get_all_1dgms(st,points)
-#print(indices)
-
-
-dgm_data0=[]
-dgm_data1=[]
-for pd in diag:
-    if pd[0]==0:
-        dgm_data0.append([pd[1][0],pd[1][1]])
-    if pd[0]==1:
-        dgm_data1.append([pd[1][0],pd[1][1]])
-dgm_data=[dgm_data0,dgm_data1]
-threshold, loop_num = s.get_threshold_1PDonly(points,dgm_data,diag,Method='PDcluster')
-print(threshold)
-
-n = len(points)  
-adjacency_matrix = csr_matrix((n, n), dtype=np.float32)   
-if Method=='VR':
-    for i in range(n):  
-        for j in range(i+1, n):  
-            dist = distance.euclidean(points[i], points[j])  
-            if dist <= threshold:  
-                adjacency_matrix[i, j] = dist  
-                adjacency_matrix[j, i] = dist 
-
-if Method=='alpha':
-    for f in Filt:
-        if len(f[0])==2:
-            i,j = f[0][0],f[0][1]
-            
-            if f[1] <= threshold: 
-                dist=distance.euclidean(points[i], points[j])
-                adjacency_matrix[i,j]=dist
-                adjacency_matrix[j,i]=dist
-            
-        
-G = nx.Graph()
-n=adjacency_matrix.shape[0]
-for i in range(n):  
-    for j in range(i+1, n):  
-        if adjacency_matrix[i, j] != 0:  
-             G.add_edges_from([(i,j)])
-
-#isolate_nodes=s.find_single_edge_vertices(adjacency_matrix)
 isolate_nodes=[1,60]
 print(isolate_nodes)
 
